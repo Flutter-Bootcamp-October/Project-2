f0e376ebe53ba0f1f5af8b4c2dcc49e79635a40c
Mohammed A <am18_8@outlook.com>
2023-10-20 21:07:52 +0300

    modified: sign in

--- project_2/lib/screen/sign_in.dart
+++ project_2/lib/screen/sign_in.dart
@@ -11,7 +11,6 @@ class SignIn extends StatefulWidget {
   @override
   State<SignIn> createState() => _SignInState();
 }
-
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
